eb9266614e20a3f1ea00f8dd5825b9fcd1c4a830
masoudMarde <mardmasoud66@gmail.com>
2024-07-28 00:43:28 +0330

    v1

---
+++ 01-expendingCards/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./app.css";
+
+function Card({ pic, expend, setExpend, index }) {
+  return (
+    <div
+      style={{ backgroundImage: `url(pics/${pic}) ` }}
+      className={expend === index ? "open" : "close"}
+      onClick={() => {
+        console.log(index);
+        setExpend(index);
+      }}
+    ></div>
+  );
+}
+function App() {
+  const pics = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+  const [expend, setExpend] = useState(3);
+
+  return (
+    <div className="container">
+      {pics.map((pic, i) => (
+        <Card
+          pic={pic}
+          key={pic}
+          i={index}
+          expend={expend}
+          setExpend={setExpend}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-recipe_page/src/ExpendingCards.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ExpendingCards() {
+  return <div>ExpendingCards</div>;
+}
